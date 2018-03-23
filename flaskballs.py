868e82719e80a1fd702948884025fff457aea7b6
James Mathis <james_mathis@mnemonicware.com>
2018-03-23 01:38:06 -0400

    Flaskballs main app

---
+++ flaskballs.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import url_for
+from flask import render_template
+from flask_assets import Bundle, Environment
+
+app = Flask(__name__)
+
+# app route for root URL #
+@app.route('/')
+
+# basic function to serve up flask HTML page #
+def index():
+	return render_template('index.html')
+
+
+
+if __name__ == "__main__":
+	app.run()
+
